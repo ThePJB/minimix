5abcb9ce7a4db20f45c30d0540bf75841569fea3
Patrick Burke <zclpatrick@gmail.com>
2023-08-15 07:41:06 +1000

    more sound work

--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,155 @@
 use cpal::traits::*;
 use ringbuf::*;
+use riff_wave::*;
+use std::io::Read;
+mod rng;
+use rng::*;
 
 pub type SoundHandle = usize;
 
+pub struct PlayingSoundHandle {
+    // generational index
+}
+pub struct SoundBuffer {
+    samples: Vec<f32>,
+}
 pub struct Channel {
-    // tstart
-    // tend
-    // repeat?
-    // stop
-    a: SoundHandle,
-    b: SoundHandle,
+    t: u64,
+    id: PlayingSoundHandle,
+    desc: SoundDesc,
+}
+
+impl Channel {
+    pub fn tick(&mut self) -> f32 {
+        if let Some(other_id) = self.desc.b {
+            let id = self.desc.a;
+            let samp_a = self.
+        }
+        self.t += 1;
+    }
 }
 
 pub struct Mixer {
-    prod: Producer<Command>,
+    rng: Rng,
+    sounds: Vec<SoundBuffer>,
+    channels: Vec<Channel>,
+    command_consumer: Consumer<Command>,
+
+}
+impl Mixer {
+    pub fn tick(&mut self) {
+
+    }
 }
 
-pub struct Command {
-    
+// mixer goes in SampleRequestOptions. now its coming back to me.
+// holy shit. maybe when im not decrepit af
+
+// needs stops as well
+
+// random u64 IDs and linear search through channels cunt. its the only way.
+
+// honestly do you just make a string for each one lol. like impact.wav/vol:0.4/fadeout:20/
+
+// almost
+// sound buffer needs random u64 as well
+
+pub enum Command {
+    Load {
+        path: String,
+        id: SoundHandle,
+    },
+    Play {
+        params: SoundDesc,
+        id: PlayingSoundHandle,
+    },
+    Stop {
+        id: PlayingSoundHandle,
+    }
+
+}
+
+// how am i gonna do handling references to the playing sounds
+// 1 channel going back from audio thread
+// 2 tell audio thread what reference im gonna use - named channels
+// 3 could do it all string based or even name based - minimal ui, kinda icky, mm but kinda based
+// 4 nah but multiple instances tho yeah.
+
+
+pub struct SoundDesc {
+    vol: f32,
+    t_begin_transition: u64,
+    t_end_transition: u64,
+    t_fade_in: u64,
+    t_fade_out: u64,
+    repeat: bool,
+    a: SoundHandle,
+    b: Option<SoundHandle>,
+}
+
+pub fn load_wav(path: &str) -> Option<Vec<f32>> {
+    let file = std::fs::File::open(path).unwrap();
+    let wave_reader = WaveReader::new(file).unwrap();
+    let format = wave_reader.pcm_format;
+    dbg!(format);
+    if format.num_channels == 1 {
+        if format.bits_per_sample == 8 {
+            return Some(wave_reader.into_inner().bytes().map(|x| x.unwrap() as f32 / 255.0 * 2.0 - 1.0).collect());
+        } 
+        if format.bits_per_sample == 16 {
+            return Some(wave_reader.into_inner().bytes().map(|x| x.unwrap() as f32 / i16::MAX as f32).collect());
+        } 
+        if format.bits_per_sample == 24 {
+            return Some(wave_reader.into_inner().bytes().map(|x| x.unwrap() as f32 / MAX_I24_VALUE as f32).collect());
+        } 
+        if format.bits_per_sample == 32 {
+            return Some(wave_reader.into_inner().bytes().map(|x| x.unwrap() as f32 / i32::MAX as f32).collect());
+        }
+    }
+    dbg!("unsupported");
+
+    None
 }
 
 pub struct SoundController {
-    cons: Consumer<Command>,
+    rng: Rng,
+    prod: Producer<Command>,
     stream: cpal::Stream,
 }
 
 impl SoundController {
     pub fn new() -> Self {
-        let rb = RingBuffer::<Sound>::new(200);
+        let rb = RingBuffer::<Command>::new(200);
         let (mut prod, mut cons) = rb.split();
         let stream = stream_setup_for(sample_next, cons).expect("no can make stream");
         stream.play().expect("no can play stream");
+        SoundController {
+            rng: Rng::new_random(),
+            sounds: vec![],
+            stream,
+            prod,
+        }
+    }
+
+    pub fn load_sound(&mut self, path: &str) -> Option<SoundHandle> {
+        let token = self.sounds.len();
+        if let Some(samples) = load_wav(path) {
+            self.sounds.push(SoundBuffer { samples });
+            return Some(token);
+        }
+        None
+    }
+
+    pub fn play(&mut self, sound: SoundHandle, vol: f32, repeat: bool) -> PlayingSoundHandle {
+
+    }
+
+    pub fn play_fade(&mut self, start: SoundHandle, end: SoundHandle, fade_start: f32, fade_end: f32, vol: f32, repeat: bool) -> PlayingSoundHandle {
+
+    }
+
+    pub fn stop(&mut self, handle: PlayingSoundHandle) {
+
     }
 }
 
